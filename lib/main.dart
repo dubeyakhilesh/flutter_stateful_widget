de1416ab3b41b45ca769f568d0dc4342bdae26eb
Akhilesh <akhilesh2011.here@gmail.com>
2019-09-26 18:07:08 +0530

    Flutter stateful widget example

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Stateful Example",
+      home: MyStateFull(),
+    );
+  }
+
+}
+
+class MyStateFull extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyFriend();
+  }
+
+}
+
+class _MyFriend extends State<MyStateFull>{
+  var myBestFriend = "";
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                "My best friend is: $myBestFriend"
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
